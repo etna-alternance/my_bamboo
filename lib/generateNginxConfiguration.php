75bc342f2eaad361f513ef942f2819f56ecc0f75
Stephane CRIVISIER <stephane@crivisier.fr>
2015-03-26 12:09:50 +0100

    fix nginx

--- lib/generateNginxConfiguration.php
+++ lib/generateNginxConfiguration.php
@@ -6,7 +6,7 @@ function generateNginxConfiguration($apps)
     $twig   = new Twig_Environment($loader);
 
     $twig->addFilter(new Twig_SimpleFilter('upstream_name', function ($string) {
-        return str_replace(".", "_", $string);
+        return str_replace([".", "-"], ["_", "_"], $string);
     }));
 
     return $twig->render("nginx.conf", ["apps" => $apps]);
